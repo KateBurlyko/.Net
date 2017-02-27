f036281113ac87be25d7f26b802695e1c63a5da7
Kate <kateeds69@gmail.com>
2017-02-27 04:47:33 +0300

    abb choose of type

---
+++ WpfApplication/WpfApplication/GetterAttribyts.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication
+{
+    class GetterAttribyts
+    {
+        public static IEnumerable<Type> GetTypes(string dllPath)
+        {
+            var dll = Assembly.LoadFile(dllPath);
+            IEnumerable<Type> types;
+            try
+            {
+                types = dll.GetTypes();
+            }
+            catch (Exception)
+            {
+                types = new List<Type>();
+            }
+            return types;
+        }
+
+        public static IEnumerable<MethodInfo> GetMethod(Type type)
+        {
+            IEnumerable<MethodInfo> methods;
+            try
+            {
+                methods = type.GetMethods();
+            }
+            catch
+            {
+                methods = new MethodInfo[0];
+            }
+            return methods;
+        }
+
+        public static IEnumerable<FieldInfo> GetFields(Type type)
+        {
+            IEnumerable<FieldInfo> fields;
+            try
+            {
+                fields = type.GetFields();
+            }
+            catch
+            {
+                fields = new FieldInfo[0];
+            }
+            return fields;
+        }
+
+        public static IEnumerable<PropertyInfo> GetProperties(Type type)
+        {
+            IEnumerable<PropertyInfo> properties;
+            try
+            {
+                properties = type.GetProperties();
+            }
+            catch
+            {
+                properties = new PropertyInfo[0];
+            }
+            return properties;
+        }
+    }
+}
--- WpfApplication/WpfApplication/MainWindow.xaml.cs
+++ WpfApplication/WpfApplication/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace WpfApplication
 {
@@ -20,9 +12,43 @@ namespace WpfApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static string Path1 { get; set; }
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Browse_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog sqw = new FolderBrowserDialog();
+            DialogResult dialogresult = sqw.ShowDialog();
+            FilePath.Text = sqw.SelectedPath;
+            DirectoryInfo dir = new DirectoryInfo(sqw.SelectedPath);
+            FileInfo[] files = dir.GetFiles("*.dll");
+            listBox.ItemsSource = files;
+            Path1 = sqw.SelectedPath;
+        }
+
+        private void listBox_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            string dllName = listBox.SelectedItem.ToString();
+            string pathToDll = $"{Path1}\\{dllName}";
+            var classesOfDll = GetterAttribyts.GetTypes(pathToDll);
+            listBoxTypes.ItemsSource = classesOfDll;
+        }
+
+        //private void radioButtonFields_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    int ty = listBoxTypes.SelectedIndex;
+        //    string pathToDll = $"{Path1}\\{dllName}";
+        //    var classesOfDll = GetterAttribyts.GetTypes(pathToDll);
+        //    GetterAttribyts.GetFields();
+           
+        //}
+
+        //private void listBoxTypes_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        //{
+
+        //}
     }
 }
