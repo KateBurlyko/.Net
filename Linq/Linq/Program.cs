170610c9b7309154cf988ab0d9d42c417c700f16
Kate <kateeds69@gmail.com>
2017-04-07 00:21:41 +0300

    add projects, add LINQ

---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Querries querries = new Querries();
+            querries.SecondQuerry();
+            querries.ThirdQuerry(10000);
+        }
+    }
+}
---
+++ Linq/Linq/Querries.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Linq
+{
+    class Querries
+    {
+        public XDocument xDoc = XDocument.Load(Resource1.XmlPath);
+        public IEnumerable<XElement> allCustomers;
+
+        public void SecondQuerry()
+        {
+            allCustomers = xDoc.Element("customers").Elements("customer");
+            var querry = from customer in allCustomers
+                         orderby customer.Element("country").Value
+                         select customer;
+            using (StreamWriter sw = new StreamWriter(Resource1.Querry2, false, Encoding.Default))
+            {
+                foreach (var someCustomer in querry)
+                {
+                    sw.WriteLine(someCustomer.Element("country").Value + " " + someCustomer.Element("name").Value);
+                }
+            }
+        }
+
+        public void ThirdQuerry(double sumX)
+        {
+            allCustomers = xDoc.Element("customers").Elements("customer");
+            var querry = from customer in allCustomers
+                         where customer.Elements("orders").Elements("order").Any(x => Double.Parse(x.Element("total").Value) > sumX)
+                         select customer;
+
+            using (StreamWriter sw = new StreamWriter(Resource1.Querry3, false, Encoding.Default))
+            {
+                sw.WriteLine("orders more than: " + sumX);
+                foreach (var someCustomer in querry)
+                {
+                    sw.WriteLine(someCustomer.Element("name").Value);
+                }
+            }
+        }
+    }
+}
